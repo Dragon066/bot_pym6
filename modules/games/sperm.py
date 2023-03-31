a3c5854ea18ae2492c13dcde5998b86f19653ebe
Kolya <NickTitov123@yandex.ru>
2023-03-31 21:16:28 +0300

    new features for sperm

--- modules/games/sperm.py
+++ modules/games/sperm.py
@@ -18,7 +18,6 @@ def save_sperm():
     try:
         with open('files/sperm.txt', 'w', encoding='utf-8') as f:
             json.dump(SPERM, f, ensure_ascii=False, indent=4)
-        log.info('SPERM сохранено')
     except Exception as err:
         log.exception('Ошибка при сохранении SPERM')
 
@@ -30,13 +29,21 @@ async def com_gets(msg):
         if id not in SPERM:
             sperm_create_default(msg)
         text = sperm_get_info_another(id)
-        await msg.reply(text)
     else:
         id = msg.from_user.id
         if id not in SPERM:
             sperm_create_default(msg)
         text = sperm_get_info(id)
-        await msg.reply(text, reply_markup=sperm_get_keyboard(id))
+    await msg.reply(text)
+
+
+@dp.message_handler(commands=['shops'])
+async def com_shops(msg):
+    id = msg.from_user.id
+    if id not in SPERM:
+        sperm_create_default(msg)
+    text = sperm_get_shop(id)
+    await msg.reply(text, reply_markup=sperm_get_keyboard(id))
 
 
 @dp.callback_query_handler(Text(startswith='sperm,'))
@@ -44,48 +51,104 @@ async def callback_sperm(call):
     if call.data.split(',')[1] == 'lvlup_power':
         id = int(call.data.split(',')[2])
         if id == call.from_user.id and SPERM[id]['power_level'] < 40:
-            if SPERM[id]['bak'] > sperm_get_price_power(id):
+            if SPERM[id]['bak'] >= sperm_get_price_power(id):
                 SPERM[id]['bak'] -= sperm_get_price_power(id)
                 SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
                 SPERM[id]['power_level'] += 1
                 save_sperm()
                 await call.answer('Вы увеличили силу dro4ки!!! 🤩')
                 await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                            text=sperm_get_info(id), reply_markup=sperm_get_keyboard(id))
+                                            text=sperm_get_shop(id), reply_markup=sperm_get_keyboard(id))
             else:
                 await call.answer('Не хватает спермы! 😡')
     elif call.data.split(',')[1] == 'lvlup_time':
         id = int(call.data.split(',')[2])
         if id == call.from_user.id and SPERM[id]['power_level'] < 40:
-            if SPERM[id]['bak'] > sperm_get_price_time(id):
+            if SPERM[id]['bak'] >= sperm_get_price_time(id):
                 SPERM[id]['bak'] -= sperm_get_price_time(id)
                 SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
                 SPERM[id]['time_level'] += 1
                 save_sperm()
                 await call.answer('Вы укрепили зависимость и теперь можете dro4ить чаще!!!!! 🤩')
                 await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text=sperm_get_info(id), reply_markup=sperm_get_keyboard(id))
+                                  text=sperm_get_shop(id), reply_markup=sperm_get_keyboard(id))
             else:
                 await call.answer('Не хватает спермы! 😡')
+    elif call.data.split(',')[1] == 'lvlup_rate':
+        id = int(call.data.split(',')[2])
+        if id == call.from_user.id and SPERM[id]['rate_level'] < 10:
+            if SPERM[id]['exp'] >= sperm_get_price_rate(id):
+                SPERM[id]['exp'] -= sperm_get_price_rate(id)
+                SPERM[id]['exp'] = round(SPERM[id]['exp'], 2)
+                SPERM[id]['rate_level'] += 1
+                save_sperm()
+                await call.answer('У вас увеличились яйца, вы стали круче 😎😎😎😎')
+                await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                  text=sperm_get_shop(id), reply_markup=sperm_get_keyboard(id))
+            else:
+                await call.answer('Не хватает опыта! 😒')
+    elif call.data.split(',')[1] == 'lvlup_skill':
+        id = int(call.data.split(',')[2])
+        if id == call.from_user.id and SPERM[id]['skill_level'] < 40:
+            if SPERM[id]['exp'] >= sperm_get_price_skill(id):
+                SPERM[id]['exp'] -= sperm_get_price_skill(id)
+                SPERM[id]['exp'] = round(SPERM[id]['exp'], 2)
+                SPERM[id]['skill_level'] += 1
+                save_sperm()
+                await call.answer('Вы повысили своё мастерство, мастер 🙏🙏')
+                await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                  text=sperm_get_shop(id), reply_markup=sperm_get_keyboard(id))
+            else:
+                await call.answer('Не хватает опыта! 😒')
 
 
 def sperm_get_keyboard(id):
     keyboard = types.InlineKeyboardMarkup()
     if SPERM[id]['power_level'] < 40:
-        button1 = types.InlineKeyboardButton(text=f'LvLUP дрочка ({sperm_get_price_power(id)}мл 🥛)', callback_data=f'sperm,lvlup_power,{id}')
-        keyboard.add(button1)
+        button = types.InlineKeyboardButton(text=f'LvLUP дрочка ({sperm_get_price_power(id)}мл 🥛)', callback_data=f'sperm,lvlup_power,{id}')
+        keyboard.add(button)
     if SPERM[id]['time_level'] < 40:
-        button2 = types.InlineKeyboardButton(text=f'LvLUP зависимость ({sperm_get_price_time(id)}мл 🥛)', callback_data=f'sperm,lvlup_time,{id}')
-        keyboard.add(button2)
+        button = types.InlineKeyboardButton(text=f'LvLUP зависимость ({sperm_get_price_time(id)}мл 🥛)', callback_data=f'sperm,lvlup_time,{id}')
+        keyboard.add(button)
+    if SPERM[id]['rate_level'] < 10:
+        button = types.InlineKeyboardButton(text=f'LvLUP яйца ({sperm_get_price_rate(id)}exp 🌀)', callback_data=f'sperm,lvlup_rate,{id}')
+        keyboard.add(button)
+    if SPERM[id]['skill_level'] < 40:
+        button = types.InlineKeyboardButton(text=f'LvLUP мастерство ({sperm_get_price_skill(id)}exp 🌀)', callback_data=f'sperm,lvlup_skill,{id}')
+        keyboard.add(button)
     return keyboard
 
+
+def sperm_get_shop(id):
+    text = f"🍆 <b>{SPERM[id]['name']}'s dick</b>\n\n" if not SPERM[id][
+        'dickname'] else f"<b>{SPERM[id]['dickname']}</b>\n\n"
+    text += f"📏 Длина вашей письки: <b>{SPERM[id]['len']} см</b>\n" \
+            f"🌀 Опыт: <b>{SPERM[id]['exp']} exp</b>\n" \
+            f"🥛 Спермобак: <b>{SPERM[id]['bak']} мл</b>\n\n" \
+            f"💪 Уровень дрочки: <b>{SPERM[id]['power_level']}</b>\n" \
+            f"💪 {':' * (SPERM[id]['power_level'] - 1)}|{'.' * (39 - SPERM[id]['power_level'])}\n" \
+            f"🕔 Уровень зависимости: <b>{SPERM[id]['time_level']}</b> <i>({sperm_get_time(id)} мин)</i>\n" \
+            f"🕔 {':' * (SPERM[id]['time_level'] - 1)}|{'.' * (39 - SPERM[id]['time_level'])}\n" \
+            f"🥚 Уровень яиц: <b>{SPERM[id]['rate_level']}</b>\n" \
+            f"🥚 {':' * (SPERM[id]['rate_level'] - 1)}|{'.' * (9 - SPERM[id]['rate_level'])}\n" \
+            f"🚼 Уровень мастерства: <b>{SPERM[id]['skill_level']}</b>\n" \
+            f"🚼 {':' * (SPERM[id]['skill_level'] - 1)}|{'.' * (39 - SPERM[id]['skill_level'])}\n"
+    return text
 
 def sperm_get_info(id):
     text = f"🍆 <b>{SPERM[id]['name']}'s dick</b>\n\n" if not SPERM[id]['dickname'] else f"<b>{SPERM[id]['dickname']}</b>\n\n"
     text += f"📏 Длина вашей письки: <b>{SPERM[id]['len']} см</b>\n" \
+           f"🌀 Опыт: <b>{SPERM[id]['exp']} exp</b>\n" \
            f"🥛 Спермобак: <b>{SPERM[id]['bak']} мл</b>\n\n" \
            f"💪 Уровень дрочки: <b>{SPERM[id]['power_level']}</b>\n" \
+           f"💪 {':'*(SPERM[id]['power_level'] - 1)}|{'.'*(39 - SPERM[id]['power_level'])}\n" \
            f"🕔 Уровень зависимости: <b>{SPERM[id]['time_level']}</b> <i>({sperm_get_time(id)} мин)</i>\n" \
+           f"🕔 {':'*(SPERM[id]['time_level'] - 1)}|{'.'*(39 - SPERM[id]['time_level'])}\n" \
+           f"🥚 Уровень яиц: <b>{SPERM[id]['rate_level']}</b>\n" \
+           f"🥚 {':'*(SPERM[id]['rate_level'] - 1)}|{'.'*(9 - SPERM[id]['rate_level'])}\n" \
+           f"🚼 Уровень мастерства: <b>{SPERM[id]['skill_level']}</b>\n" \
+           f"🚼 {':'*(SPERM[id]['skill_level'] - 1)}|{'.'*(39 - SPERM[id]['skill_level'])}\n" \
+           f"🍌 Количество мастурбаций: <b>{SPERM[id]['masturbate_count']}</b>\n" \
            f"💨 Количество CUMчей: <b>{SPERM[id]['cum_count']}</b> 💦\n\n"
     if dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(minutes = sperm_get_time(id)) < dt.datetime.now():
         text += f"❗️ <b>Время подрочить! /masturbate</b>"
@@ -93,8 +156,8 @@ def sperm_get_info(id):
         time = str(dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(minutes = sperm_get_time(id)) - dt.datetime.now())
         time = time[:time.find('.')]
         text += f"⏳ <i>До следующей dro4ки: {time}</i>"
-    if SPERM[id]['len'] * 0.8 > 1:
-        text += f"\n\n<b>Вы можете кончить /cum</b>\n(потеря 20% длины, пополнение spermобака, вместо dro4ки)"
+    if SPERM[id]['len'] * 0.8 > 1 and (dt.datetime.fromtimestamp(int(SPERM[id]['cum_time'])) + dt.timedelta(minutes = sperm_get_time(id)) < dt.datetime.now()):
+        text += f"\n\n<b>Вы можете кончить /cum</b>\n(потеря 20% длины, пополнение 10% spermобака)"
     else:
         text += f"\n\n🤏 Подростите письку и сможете кончить!"
     if not SPERM[id]['dickname']:
@@ -105,30 +168,63 @@ def sperm_get_info(id):
 def sperm_get_info_another(id):
     text = f"🍆 <b>{SPERM[id]['name']}'s dick</b>\n\n" if not SPERM[id][
         'dickname'] else f"<b>{SPERM[id]['dickname']}</b>\n\n"
-    text += f"📏 Длина вашей письки: <b>{SPERM[id]['len']} см</b>\n" \
-            f"🥛 Спермобак: <b>{SPERM[id]['bak']} мл</b>\n\n" \
-            f"💪 Уровень дрочки: <b>{SPERM[id]['power_level']}</b>\n" \
-            f"🕔 Уровень зависимости: <b>{SPERM[id]['time_level']}</b> <i>({sperm_get_time(id)} мин)</i>\n" \
-            f"💨 Количество CUMчей: <b>{SPERM[id]['cum_count']}</b> 💦\n\n"
+    text += f"🌀 Опыт: <b>{SPERM[id]['exp']} exp</b>\n" \
+           f"🥛 Спермобак: <b>{SPERM[id]['bak']} мл</b>\n\n" \
+           f"💪 Уровень дрочки: <b>{SPERM[id]['power_level']}</b>\n" \
+           f"💪 {':'*(SPERM[id]['power_level'] - 1)}|{'.'*(39 - SPERM[id]['power_level'])}\n" \
+           f"🕔 Уровень зависимости: <b>{SPERM[id]['time_level']}</b> <i>({sperm_get_time(id)} мин)</i>\n" \
+           f"🕔 {':'*(SPERM[id]['time_level'] - 1)}|{'.'*(39 - SPERM[id]['time_level'])}\n" \
+           f"🥚 Уровень яиц: <b>{SPERM[id]['rate_level']}\n</b>" \
+           f"🥚 {':'*(SPERM[id]['rate_level'] - 1)}|{'.'*(9 - SPERM[id]['rate_level'])}\n" \
+           f"🚼 Уровень мастерства: <b>{SPERM[id]['skill_level']}</b>\n" \
+           f"🚼 {':'*(SPERM[id]['skill_level'] - 1)}|{'.'*(39 - SPERM[id]['skill_level'])}\n" \
+           f"🍌 Количество мастурбаций: <b>{SPERM[id]['masturbate_count']}</b>\n" \
+           f"💨 Количество CUMчей: <b>{SPERM[id]['cum_count']}</b> 💦\n\n"
     return text
 
 
 def sperm_get_price_power(id):
-    return round(1.5 * math.exp(SPERM[id]['power_level'] / 40 + 1) - 3 * (1 / (SPERM[id]['power_level'] + 1)), 2)
+    n = SPERM[id]['power_level'] + 1
+    return round(0.5 * n + (n / 10) ** 1.85535, 2)
 
 
 def sperm_get_price_time(id):
-    return round(1.25 * math.exp(SPERM[id]['time_level'] / 40 + 1) - 3 * (1 / (SPERM[id]['time_level'] + 1)), 2)
+    n = SPERM[id]['time_level'] + 1
+    return round(0.25 * n + (n / 7) ** 1.9051, 2)
+
+
+def sperm_get_price_rate(id):
+    n = SPERM[id]['rate_level'] + 1
+    return round((9.9259 + 1.5 * n) ** math.e)
+
+
+def sperm_get_price_skill(id):
+    n = SPERM[id]['skill_level'] + 1
+    return round(10 + 11 * n + (n / 2) ** 2.003152)
+
+
+def sperm_get_rate(id):
+    return SPERM[id]['rate'] * (1 + 0.1 * SPERM[id]['rate_level'])
 
 
 def sperm_get_time(id):
     return 30 - SPERM[id]['time_level'] * 0.5
 
 
+def sperm_get_exp(id):
+    rand = rnd.uniform(1 + 0.25 * SPERM[id]['rate_level'], 10 + 0.25 * SPERM[id]['rate_level'])
+    return round(sperm_get_rate(id) * rand)
+
+
 def sperm_get_len(id):
-    from_ = 0.05 + 0.1 * SPERM[id]['power_level']
+    from_ = 0.05 + 0.05 * SPERM[id]['power_level']
     to_ = 0.5 + 0.1 * SPERM[id]['power_level']
-    return round(rnd.uniform(from_, to_), 2)
+    return round(sperm_get_rate(id) * rnd.uniform(from_, to_), 2)
+
+
+def sperm_get_random(id):
+    n = SPERM[id]['skill_level']
+    return rnd.uniform(0, 1) <= (5 + 1.125 * n) / 100
 
 
 def sperm_create_default(msg):
@@ -139,11 +235,18 @@ def sperm_create_default(msg):
         'name': name,
         'len': 1,
         'bak': 0,
+        'exp': 0,
         'dickname': None,
         'time': 0,
+        'cum_time': 0,
         'time_level': 0,
         'power_level': 0,
-        'cum_count': 0
+        'skill_level': 0,
+        'rate_level': 0,
+        'rate': 1,
+        'rate_time': 0,
+        'cum_count': 0,
+        'masturbate_count': 0
     }
     save_sperm()
 
@@ -151,24 +254,35 @@ def sperm_create_default(msg):
 @dp.message_handler(commands=['cum'])
 async def com_cum(msg):
     id = msg.from_user.id
-    if dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(
+    if dt.datetime.fromtimestamp(int(SPERM[id]['cum_time'])) + dt.timedelta(
             minutes=sperm_get_time(id)) < dt.datetime.now():
         if SPERM[id]['len'] * 0.8 > 1:
-            SPERM[id]['time'] = dt.datetime.timestamp(dt.datetime.now())
-            toadd = round(SPERM[id]['len'] * 0.2 * 0.5, 2)
-            SPERM[id]['len'] = round(SPERM[id]['len'] * 0.8, 2)
+            SPERM[id]['cum_time'] = dt.datetime.timestamp(dt.datetime.now())
+            tominus = round(SPERM[id]['len'] * 0.2, 2)
+            if not(sperm_get_random(id)):
+                toadd = round(sperm_get_rate(id) * SPERM[id]['len'] * 0.2 * 0.5, 2)
+                SPERM[id]['len'] = round(SPERM[id]['len'] * 0.8, 2)
+                SPERM[id]['bak'] += toadd
+                SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
+                await msg.reply(
+                    f'😲😲😲 <b>ВЫ КОНЧИЛИ!!!!</b> 💦💦💦\n\n<b>+{toadd} мл</b> в spermобак 🥛\n\nОставшаяся длина писюнчика: <b>{SPERM[id]["len"]} см</b> (-{tominus}см)')
+            else:
+                toadd = round(2 * sperm_get_rate(id) * SPERM[id]['len'] * 0.2 * 0.5, 2)
+                SPERM[id]['len'] = round(SPERM[id]['len'] * 0.8, 2)
+                SPERM[id]['bak'] += toadd
+                SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
+                await msg.reply(
+                    f'😲😲😲 <b>МЕГА КАМШОТ Х2!!!!</b> 💦💦💦\n<i>Ваше мастерство поражает!</i>\n<b>+{toadd} мл</b> в spermобак 🥛\n\nОставшаяся длина писюнчика: <b>{SPERM[id]["len"]} см</b> (-{tominus}см)')
             SPERM[id]['cum_count'] += 1
-            SPERM[id]['bak'] += toadd
-            SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
             save_sperm()
-            await msg.reply(f'😲😲😲 <b>ВЫ КОНЧИЛИ!!!!</b> 💦💦💦\n\n<b>+{toadd} мл</b> в spermобак 🥛\n\nОставшаяся длина писюнчика: <b>{SPERM[id]["len"]} см</b>')
+
         else:
             await msg.reply('🤏🤏🤏 У вас слишком маленький писюн, чтобы кончить :( хаха')
     else:
         time = str(dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(
             minutes=sperm_get_time(id)) - dt.datetime.now())
         time = time[:time.find('.')]
-        await msg.reply(f"До следующей дрочки: <b>{time}</b>")
+        await msg.reply(f"До следующего камшота: <b>{time}</b>")
 
 
 @dp.message_handler(commands=['masturbate'])
@@ -179,12 +293,24 @@ async def com_masturbate(msg):
             sperm_create_default(msg.reply_to_message)
         if dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(minutes=sperm_get_time(id)) < dt.datetime.now():
             SPERM[id]['time'] = dt.datetime.timestamp(dt.datetime.now())
-            toadd = sperm_get_len(id)
+            toadd = round(sperm_get_rate(id) * sperm_get_len(id), 2)
             SPERM[id]['len'] += toadd
             SPERM[id]['len'] = round(SPERM[id]['len'], 2)
+            add_exp = sperm_get_exp(id)
+            SPERM[id]['exp'] += add_exp
+            SPERM[id]['masturbate_count'] += 1
+            SPERM[msg.from_user.id]['exp'] += round(add_exp * 0.2)
             await msg.reply(f"<b>{msg.from_user.first_name}</b> подрочил <b>{msg.reply_to_message.from_user.first_name}</b>! ❤️❤️❤️\n\n"
                             f"📏 Писька <b>{msg.reply_to_message.from_user.first_name}</b> увеличилась на <b>{toadd} см</b>\n"
-                            f"Теперь длина этого dick'a: <b>{SPERM[id]['len']}</b> 👀")
+                            f"Теперь длина этого dick'a: <b>{SPERM[id]['len']}</b> 👀\n\n"
+                            f"Also, <b>+{add_exp} exp 🌀</b> для <b>{msg.reply_to_message.from_user.first_name}</b> и"
+                            f" <b>+{round(add_exp * 0.2)}</b> для <b>{msg.from_user.first_name}</b>")
+            if sperm_get_random(id):
+                SPERM[id]['bak'] += toadd * 0.5
+                SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
+                await msg.reply(f"ВЫ ЗАСТАВИЛИ <b>{msg.reply_to_message.from_user.first_name}</b> ПОТЕЧЬ УФФФФ 💦💦💦💦\n\n"
+                                f"Вы, как истинный ценитель dro4ки, помогли собрать кончу в баночку:\n"
+                                f"<b>+{round(toadd * 0.5, 2)} мл</b> в spermoбак 🥛")
             save_sperm()
         else:
             time = str(dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(
@@ -197,11 +323,21 @@ async def com_masturbate(msg):
             sperm_create_default(msg)
         if dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(minutes=sperm_get_time(id)) < dt.datetime.now():
             SPERM[id]['time'] = dt.datetime.timestamp(dt.datetime.now())
-            toadd = sperm_get_len(id)
+            toadd = round(sperm_get_rate(id) * sperm_get_len(id), 2)
             SPERM[id]['len'] += toadd
             SPERM[id]['len'] = round(SPERM[id]['len'], 2)
-            await msg.reply(f"Ох хорошо (чудесно)! 😪\n\n📏 Ваша писька увеличилась на <b>{toadd} см</b>\n"
-                            f"❤️ Теперь длина вашего 4leна: <b>{SPERM[id]['len']}</b>")
+            add_exp = sperm_get_exp(id)
+            SPERM[id]['exp'] += add_exp
+            SPERM[id]['masturbate_count'] += 1
+            await msg.reply(f"Ох хорошо! 😪\n\n📏 Ваша писька увеличилась на <b>{toadd} см</b>\n"
+                            f"❤️ Теперь длина вашего 4leна: <b>{SPERM[id]['len']}\n\n</b>"
+                            f"Also, +{add_exp} exp 🌀")
+            if sperm_get_random(id):
+                SPERM[id]['bak'] += toadd * 0.5
+                SPERM[id]['bak'] = round(SPERM[id]['bak'], 2)
+                await msg.reply(f"Вы потекли 💦💦😢\n\n"
+                                f"Но это даже хорошо! Вы немного, конечно же, случайно, кончили:\n"
+                                f"<b>+{round(toadd * 0.5, 2)} мл</b> в spermoбак 🥛")
             save_sperm()
         else:
             time = str(dt.datetime.fromtimestamp(int(SPERM[id]['time'])) + dt.timedelta(
